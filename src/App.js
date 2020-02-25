4645a0f1de4ed5317e6a87b5e3647cb50b6514bd
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-25 10:10:51 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [trueOfFalse, setTrueOrFalse] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{trueOfFalse === true ? <h1>I'm true</h1> : <h1>I'm false</h1>}</div>
   );
-}
+};
 
 export default App;
